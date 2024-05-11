30b4bc1685d0ce1fab40b925c741bd4f98403374
maxinms2 <you@example.com>
2024-05-11 17:13:20 -0600

    patron visitor

--- src/test/java/patrones/PatronesApplicationTests.java
+++ src/test/java/patrones/PatronesApplicationTests.java
@@ -1,14 +1,34 @@
 package patrones;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import patrones.visitor.spring.Circle;
+import patrones.visitor.spring.ClienteVisitor;
+import patrones.visitor.spring.Figure;
+import patrones.visitor.spring.FigureVisitor;
+import patrones.visitor.spring.Square;
 
 @SpringBootTest
 class PatronesApplicationTests {
-
+	@Autowired
+	private FigureVisitor visitor;
+	
 	@Test
-	void contextLoads() {
-		assert (1==1);
+	void visitor() {
+		Circle c= new Circle(); Square sq=new Square();
+	    List<Figure> figures=new ArrayList<>();
+	    figures.add(c); figures.add(sq);
+		ClienteVisitor cliente=new ClienteVisitor(figures, visitor);
+		
+        StringBuilder sb = new StringBuilder("Results:\n");
+        figures.forEach(figure -> figure.accept(visitor));
+         sb.toString();
 	}
 	
 
